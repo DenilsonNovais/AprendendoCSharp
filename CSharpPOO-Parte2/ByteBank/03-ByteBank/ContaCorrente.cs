f593f212c0294a9015959dda76b4b8263c116218
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-11-25 01:39:17 -0300

    Referencia ao mesmo objeto

---
+++ CSharpPOO-Parte2/ByteBank/03-ByteBank/ContaCorrente.cs
@@ -0,0 +1,13 @@
+﻿
+
+
+//A Class é o molde para se construir o objeto
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+}
+
+
