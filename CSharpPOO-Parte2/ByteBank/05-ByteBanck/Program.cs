1dd62bdfabc4443a77dc9164c205567f4ed43316
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-11-26 12:21:04 -0300

    Entendendo namespace e criando nova class

---
+++ CSharpPOO-Parte2/ByteBank/05-ByteBanck/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBanck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Cliente gabriela = new Cliente();
+
+            gabriela.nome = "Gabriela";
+            gabriela.profissao = "Desenvolvedora C#";
+            gabriela.cpf = "434.562.878-20";
+
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = gabriela;
+            conta.saldo = 500;
+            conta.numero = 563;
+            conta.agencia = 5634527;
+
+            Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
