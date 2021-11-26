c1ef1bbefc8e62d6577f4ef6255fbcd1e3d0d116
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-11-26 11:23:12 -0300

    Ciando Função Transferir

--- CSharpPOO-Parte2/ByteBank/04-ByteBank/Program.cs
+++ CSharpPOO-Parte2/ByteBank/04-ByteBank/Program.cs
@@ -13,14 +13,35 @@ namespace _04_ByteBank
             ContaCorrente contaDoBruno = new ContaCorrente();
 
             contaDoBruno.titular = "Bruno";
-
             Console.WriteLine(contaDoBruno.saldo);
+
+
             bool resultadoSaque = contaDoBruno.Sacar(500);
             Console.WriteLine(contaDoBruno.saldo);
             Console.WriteLine(resultadoSaque);
 
             contaDoBruno.Depositar(500);
             Console.WriteLine(contaDoBruno.saldo);
+
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Conta do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Conta do Gabriela: " + contaDaGabriela.saldo);
+
+            bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Conta do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Conta do Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine("Resultado da Transferencia: " + resultadoTransferencia);
+
+            contaDaGabriela.Transferir(100, contaDoBruno);
+
+            Console.WriteLine("Conta do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Conta do Gabriela: " + contaDaGabriela.saldo);
 
             Console.ReadLine();
         }
