08976d1ec311272a33d273bbf2d9a863723e9741
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2021-11-26 10:22:41 -0300

    Estudando Funções em CSharp
    Criando função Sacar do Banco ByteBank!

---
+++ CSharpPOO-Parte2/ByteBank/04-ByteBank/ContaCorrente.cs
@@ -0,0 +1,28 @@
+﻿
+
+
+//A Class é o molde para se construir o objeto
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numero;
+    public double saldo = 100;
+
+    public bool Sacar (double valor)
+    {
+        if (this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            return true;
+        }
+    }
+
+
+}
+
+
